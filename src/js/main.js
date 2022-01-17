e35b82fabff948e5d10e05a31e2e662324450b3a
iljas.bezhanidze <iljas.bezhanidze@totonis.com>
2022-01-17 16:59:50 +0200

    slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,28 @@
 //disabled link
 document.querySelector('.b-menu__link_disabled').onclick = e => e.preventDefault()
+
+//sliders
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 100,
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: 'custom',
+        renderCustom: function (swiper, current, total) {
+            if (current < 10) {
+                total = '0' + total
+            }
+            if (total < 10) {
+                current = '0' + current
+            }
+            return current + ' ... ' + (total);
+        }
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
